eefe653871d8798039b9dd2307f9a362eb22c28e
Matthew Wilmsmeyer <mwilmsm@gmail.com>
2018-09-28 23:59:02 -0500

    BLU-2-random-movement Added random movement script, added enemy, applied random movement to ally and enemy. -mw

---
+++ Assets/Scripts/RandomMovementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovementScript : MonoBehaviour
+{
+
+    public float walkSpeed = 10.0f;
+    
+    private Rigidbody2D rb2d;
+    private float randomDirectionTimer;
+    public float timeUntilDirectionChange = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.rb2d = gameObject.GetComponent<Rigidbody2D>();
+        this.MoveRandomly();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > this.randomDirectionTimer + this.timeUntilDirectionChange)
+        {
+            this.MoveRandomly();
+        }
+    }
+
+    void MoveRandomly()
+    {
+        Vector2 velocity = new Vector2(0, 0);
+        while (velocity == new Vector2(0, 0))
+        {
+            velocity = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+        }
+        this.rb2d.velocity = velocity;
+        this.randomDirectionTimer = Time.time;
+    }
+}
