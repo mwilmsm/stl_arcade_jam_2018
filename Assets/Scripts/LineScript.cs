f8b10d967c3d22be654f509dac943f94d1eee53b
Matthew Wilmsmeyer <mwilmsm@gmail.com>
2018-09-29 03:26:41 -0500

    BLU-5 LineScript now has code to linecast and a 'safe zone' variable to see the enemy is in the middle of the line or not. -mw

--- Assets/Scripts/LineScript.cs
+++ Assets/Scripts/LineScript.cs
@@ -7,6 +7,7 @@ public class LineScript : MonoBehaviour {
 	public GameObject gameObject2;          // Reference to the second GameObject
  
 	private LineRenderer line;                           // Line Renderer
+    public float safeZoneRadius;                  // as a percentage - the safe zone is 0.5 +/- safeZoneRadius
  
 	// Use this for initialization
 	void Start () {
@@ -23,9 +24,26 @@ public class LineScript : MonoBehaviour {
 		// Check if the GameObjects are not null
 		if (gameObject1 != null && gameObject2 != null)
 		{
-			// Update position of the two vertex of the Line Renderer
-			line.SetPosition(0, gameObject1.transform.position);
-			line.SetPosition(1, gameObject2.transform.position);
+            Vector2 start = gameObject1.transform.position;
+            Vector2 end = gameObject2.transform.position;
+
+            // Update position of the two vertex of the Line Renderer
+            line.SetPosition(0, start);
+			line.SetPosition(1, end);
+
+            this.CheckForCollisions(start, end);
 		}
 	}
+
+    void CheckForCollisions(Vector2 start, Vector2 end)
+    {
+        // Check if enemy is hitting the line
+        RaycastHit2D raycast = Physics2D.Linecast(start, end, 1 << LayerMask.NameToLayer("Enemy"));
+        if (raycast.collider != null
+            && (raycast.fraction > (0.5 + this.safeZoneRadius)
+            || raycast.fraction < (0.5 - this.safeZoneRadius)))
+        {
+            Debug.Log("Hit the enemy! " + Time.time);
+        }
+    }
 }
